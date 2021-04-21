5354dbc54d9d228162ae01933443386a3846e022
Ghasak <mdotgh@gmail.com>
2021-04-21 19:42:47 +0900

    Formulating the travel distance

--- src/s01_sketch/sketch.js
+++ src/s01_sketch/sketch.js
@@ -5,8 +5,10 @@
   ▒█░░▒█ ▀░░▀ ▀▀▀ ▀░░▀ 　 ▒█░░░ ▀▀▀ ▀░░▀ ░░▀░░ ▀░░ ▀▀▀▀ ▀░▀▀ ▀░░░▀
  *
  */
-const requiredTime = 10000; // 1 sec = 1000 millisec.
+const requiredTime = 10; // 1 sec = 1000 millisec.
 let travelDistance;
+let timeTraveled;
+timeTraveled = 0;
 const px1 = 200;
 const py1 = 300;
 const px2 = 500;
@@ -20,11 +22,14 @@ const yc = py2 - py1;
 
 travelDistance = Math.sqrt((px2 - px1) ** 2 + (py2 - py1) ** 2);
 
-let vx = (travelDistance / requiredTime) * 1000; // 1000 millisecond
-let vy = (travelDistance / requiredTime) * 1000;
+let vx = travelDistance / (requiredTime * 1000); // 1000 millisecond
+let vy = travelDistance / (requiredTime * 1000);
 let calculateTime;
 calculateTime = 0;
 const fps = 60;
+let switchx;
+switchx = false;
+let stoppingTime;
 
 function setup() {
   /** It will used for setup your objects
@@ -45,6 +50,7 @@ function draw() {
   particleX();
   checkBoudaries();
   timeelapsed();
+  arrivalParticle();
 }
 
 function screenStats() {
@@ -73,6 +79,23 @@ function checkBoudaries() {
   }
 }
 
+function arrivalParticle() {
+  if (distanceX >= px2 && distanceY >= py2) {
+    // textSize(20);
+    switchx = true;
+    stoppingTime = timeTraveled;
+  }
+
+  if (switchx === true) {
+    text(
+        'Arrive at : ' + (stoppingTime / 1000).toFixed(2) + ' Sec. ',
+        500,
+        600,
+    );
+    // textSize(10);
+  }
+}
+
 function particleX() {
   fill([123, 100, 140]);
   ellipse(200, 300, 100, 100);
@@ -80,17 +103,16 @@ function particleX() {
   ellipse(500, 500, 100, 100);
   const ld = normalizeVector(px2, py2, px1, px2);
 
-  distanceX = distanceX + (vx * (xc / ld) * deltaTime) / 1000;
-  distanceY = distanceY + (vy * (yc / ld) * deltaTime) / 1000;
+  distanceX = distanceX + vx * (xc / ld) * deltaTime;
+  distanceY = distanceY + vy * (yc / ld) * deltaTime;
 
   fill([120, 150, 144]);
   ellipse(
       distanceX,
       distanceY,
-      50 + Math.sin(parseFloat(frameCount / 10) * 30),
-      50 + Math.sin(parseFloat(frameCount / 10) * 30),
+      50 + sin(frameCount / 10) * 20,
+      50 + sin(frameCount / 10) * 20,
   );
-  console.log(50 + Math.sin(parseFloat(frameCount / 10) * 300));
   fill(10, 10, 10);
 
   text(
@@ -113,5 +135,11 @@ function timeelapsed() {
       'Ellapsed Time: ' + (fc / fps).toFixed(3),
       distanceX + 40,
       distanceY + 40,
+  );
+  timeTraveled = timeTraveled + deltaTime;
+  text(
+      'Real Time Traveled (sec): ' + (timeTraveled / 1000).toFixed(2),
+      distanceX + 50,
+      distanceY + 50,
   );
 }
